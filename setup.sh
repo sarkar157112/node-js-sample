b9c7e1fc26f3c4e71743f734ce4b6eccdff07b03
sarkar157112 <69862614+sarkar157112@users.noreply.github.com>
2022-06-29 22:16:24 +0530

    Update setup.sh
    ff

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-chmod +x /home/extranceuat/extrance-backend-api-pipeline/uat-api_setup.sh
+chmod +x /home/ubuntu/setup.sh
 #aws s3 sync s3://bb-pipeline-files/bb-sslkeys/ /home/ubuntu/wildsslkeys
 #chown -R ubuntu:ubuntu /home/ubuntu/wildsslkeys
-cd /home/extranceuat/extrance-backend-api-pipeline && \
+cd /home/ubuntu && \
 tar -xvf deploy.tar.gz &&  \
-chown -R extranceuat:extranceuat /home/extranceuat/extrance-backend-api-pipeline
+chown -R ubuntu:ubuntu /home/ubuntu/node-js-sample
 #aws s3 cp s3://bb-pipeline-files/admin-node-api-prod/.env .
-rm -rf /home/extranceuat/extrance-backend-api-pipeline/deploy.tar.gz
+rm -rf /home/ubuntu/node-js-sample/deploy.tar.gz
